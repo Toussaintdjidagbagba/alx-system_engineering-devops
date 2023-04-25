0a4d0e33ea50606248476181527a38e067a0e083
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-04-26 00:20:29 +0100

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.1.0
+# install flask -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/pip3 install puppet-lint -v 2.1.0',
+exec { 'flask':
+  command => '/usr/bin/pip3 install flask -v 2.1.0',
 }
